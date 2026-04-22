9810bc452cb720312b3a8589e07f63e4c765175d
kdg <daegoo123@gmail.com>
2026-04-22 17:36:18 +0900

    자바 KB 숙제

---
+++ src/additionalQuestions/practice0422/payment_student/PayException.java
@@ -0,0 +1,14 @@
+package additionalQuestions.practice0422.payment_student;
+
+import java.lang.Exception;
+
+// 사용자 정의 Exception : 수정하지 말고 그대로 사용하세요.
+public class PayException extends Exception {
+	
+	public PayException(String message){
+	    super(message);
+
+	}
+
+
+}
