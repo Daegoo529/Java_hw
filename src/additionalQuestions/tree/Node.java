cea8af4e785726f95bef20017aa90bbc8d7d2b08
kdg <daegoo123@gmail.com>
2026-04-29 16:34:20 +0900

    feat : additionalQuestion.tree - 문제 추가,풀이

---
+++ src/additionalQuestions/tree/Node.java
@@ -0,0 +1,12 @@
+package additionalQuestions.tree;
+
+public class Node{
+    Node left;
+    Node right;
+    int value;
+    public Node(int data) {
+        this.value = data;
+        this.left = null;
+        this.right = null;
+    }
+}
